89f80f7997b083b2ad8f105be0a1d7efb1efe1a1
Matteo-Pietro-Pillitteri <pillitterimatteo@hotmail.it>
2024-02-13 12:22:17 +0100

    Bisenetv1_pruned file makes sure to save both empty and fully filters

--- eval/eval_iou.py
+++ eval/eval_iou.py
@@ -24,7 +24,7 @@ from erfnet import ERFNet
 from bisenetv1 import BiSeNetV1
 from transform import Relabel, ToLabel, Colorize
 from iouEval import iouEval, getColorEntry
-from bisenetv1_pruned_conv_avg import BiSeNetV1_pruned
+from AnomalySegmentation_AML.eval.bisenetv1_pruned import BiSeNetV1_pruned
 
 NUM_CHANNELS = 3
 NUM_CLASSES = 20
