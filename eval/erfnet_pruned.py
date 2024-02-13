167dbbe5b9c15c945f2deb02f5ee15581fa8f2c0
Matteo-Pietro-Pillitteri <pillitterimatteo@hotmail.it>
2024-02-13 21:52:06 +0100

    Change in Erfnet pruned

--- eval/erfnet_pruned.py
+++ eval/erfnet_pruned.py
@@ -13,7 +13,7 @@ class DownsamplerBlock (nn.Module):
     def __init__(self, ninput, noutput):
         super().__init__()
 
-        self.conv = nn.Conv2d(ninput, noutput-ninput, (3, 3), stride=2, padding=1, bias=True)
+        self.conv = nn.Conv2d(ninput, noutput, (3, 3), stride=2, padding=1, bias=True)
         self.pool = nn.MaxPool2d(2, stride=2)
         self.bn = nn.BatchNorm2d(noutput, eps=1e-3)
 
@@ -64,7 +64,7 @@ class non_bottleneck_1d (nn.Module):
 class Encoder(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
-        self.initial_block = DownsamplerBlock(3,8)
+        self.initial_block = DownsamplerBlock(1,8)
 
         self.layers = nn.ModuleList()
 
@@ -134,7 +134,7 @@ class Decoder (nn.Module):
         return output
 
 
-class ERFNet(nn.Module):
+class ERFNet_pruned(nn.Module):
     def __init__(self, num_classes, encoder=None):  #use encoder to pass pretrained encoder
         super().__init__()
 
